663b0faf795c158ddcd27076034391e654683dc6
SirSouza <sr.souza.contato@gmail.com>
2026-02-03 22:04:50 -0300

    change img to Image to a better optimization

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'skillicons.dev',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.jsdelivr.net',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
